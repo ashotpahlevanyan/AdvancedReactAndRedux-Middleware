f3c0d58333c8794711c328ff183a7debcf3694cc
Ashot Pahlevanyan <ashotpahlevanyan@gmail.com>
2018-04-25 19:48:24 +0400

    added async middleware that will run the cycle till it receives the data, and loads a new action with the same time and payload as that data and dispatches a new action

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,9 +1,21 @@
 export default function({ dispatch }) {
-	return function(next) {
-		return function(action) {
-			console.log(action);
+	return next => action => {
+		console.log(action);
+		// if the action doesn't have a payload,
+		// or if the payload doesn't have a property .then,
+		// we don't care a bout it, send it on
+		if (!action.payload || !action.payload.then) {
+			return next(action);
+		}
 
-			next(action);
-		};
+		// Make sure the action's promise resolves
+
+		action.payload
+			.then(function (response) {
+				// Create a new action with the old type, but
+				// replace the promise with the response data
+				const newAction = { ...action, payload: response };
+				dispatch(newAction);
+			})
 	};
 }
